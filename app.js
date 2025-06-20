4190fb0906b0d7ad85f38ddac464d8ce9fc9e295
LaerteDelmondes <delmondesoliveiralaerte@gmail.com>
2025-06-20 11:44:46 -0300

    Corrigindo Loop Infinito

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function sortear() {
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
 
-    if (de >= ate) {
-    alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado no campo "Do número" até o campo "Até o número". Verifique!');
     return;
   }
     
